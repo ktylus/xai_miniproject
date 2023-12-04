b5d8c4c9d7619dbf98ba4b078a941b601cb99660
ktylus <karaxek23@gmail.com>
2023-12-04 10:35:26 +0100

    (incomplete) added neighbourhood metrics

--- metrics.py
+++ metrics.py
@@ -8,6 +8,36 @@ def calculate_point_fidelity(
     return torch.pow(surrogate_model_pred - base_model_pred, 2)
 
 
+def calculate_global_fidelity(
+        base_model_pred,
+        surrogate_model_pred
+):
+    n_elements = base_model_pred.shape[0]
+    return (1 / n_elements) * torch.sum(calculate_point_fidelity(base_model_pred, surrogate_model_pred), dim=-1)
+
+
+def calculate_neighborhood_fidelity(
+        base_model,
+        surrogate_model,
+        x,
+        perturbation_variance,
+        n_points
+):
+    #TODO
+    pass
+
+
+def calculate_global_neighborhood_fidelity(
+        base_model,
+        surrogate_model,
+        x,
+        perturbation_variance,
+        n_points
+):
+    return torch.sum(calculate_neighborhood_fidelity(base_model, surrogate_model, x,
+                                                     perturbation_variance, n_points), dim=-1)
+
+
 # x's shape - [batch_size, features]
 def mtl_loss(
         x,
