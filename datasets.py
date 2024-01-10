dd91079b0da68124907f9092c09249332646eaea
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2024-01-10 16:52:07 +0100

    Add AutoMpgDataset

--- datasets.py
+++ datasets.py
@@ -250,3 +250,96 @@ class WineDataset(Dataset):
         features = self.features.iloc[idx].values
         target = self.target.iloc[idx]
         return features, target
+
+
+class AutoMpgDataset(Dataset):
+    """Loads and contains auto mpg dataset
+
+    http://archive.ics.uci.edu/dataset/9/auto+mpg
+
+    Example usage:
+        from torch.utils.data import DataLoader
+
+        auto_path = "data/auto_mpg/auto-mpg.data"
+
+        test_dataset =  AutoMpgDataset(auto_path, normalize=True, train=False)
+        train_dataset = AutoMpgDataset(auto_path, normalize=True, train=True)
+
+        test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+        train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+    Attributes:
+        target_col: int. Index of target column in the dataset
+        dataset: pd.DataFrame. Whole dataset
+        features: pd.DataFrame. Data containing features
+        target: pd.DataFrame. Data containing targets
+    """
+
+    def __init__(
+        self,
+        dataset_path: str,
+        normalize: bool = False,
+        train: bool = True,
+        train_size: float = 0.8,
+    ):
+        """Initializes dataset
+
+        Args:
+            dataset_path: Path to auto-mpg.data
+            normalize: Boolean whether to normalize dataset to mean=0 and std=1
+            train: Boolean whether to extract training set
+            train_size: Fraction of the dataset devoted to the training set
+        """
+        dataset = pd.read_csv(dataset_path, header=None, delim_whitespace=True)
+        dataset = self._preprocess_dataset(dataset)
+
+        self.target_col = dataset.columns[0]  # 0, mpg - miles per galon
+        self.features, self.target = self._split_features_target(dataset)
+
+        if normalize:
+            self._normalize_dataset()
+        features_train, features_test, target_train, target_test = train_test_split(
+            self.features, self.target, train_size=train_size
+        )
+        if train:
+            self.features = features_train
+            self.target = target_train
+        else:
+            self.features = features_test
+            self.target = target_test
+
+    def _preprocess_dataset(self, dataset):
+        """Drops car model name column and deletes rows that contain '?' value"""
+        dataset = dataset.drop(
+            dataset.columns[-1], axis=1
+        )  # Drop column with car model name (unique string)
+
+        dataset.dropna()
+        for col in dataset.columns:
+            unique_values = dataset[col].unique()
+            if "?" in unique_values:
+                col_name = col
+
+        dataset = dataset[dataset[col_name] != "?"]
+        return dataset
+
+    def _normalize_dataset(self):
+        """Normalizes the dataset to mean=0 and std=1"""
+        scaler = StandardScaler()
+        self.features = pd.DataFrame(scaler.fit_transform(self.features))
+
+    def _split_features_target(self, dataset):
+        """Splits the dataset into features and target"""
+        features = dataset.drop(self.target_col, axis=1)
+        target = dataset[self.target_col]
+        return features, target
+
+    def __len__(self):
+        """Returns the length of the dataset"""
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        """Returns the features and target for a given index"""
+        features = self.features.iloc[idx].values
+        target = self.target.iloc[idx]
+        return features, target
