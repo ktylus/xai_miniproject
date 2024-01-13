573f69cf45339a702c5f8738851c912e83692bf1
ktylus <karaxek23@gmail.com>
2024-01-13 17:31:55 +0100

    divided models into classifiers and regressors

--- models/base_model.py
+++ models/base_model.py
@@ -2,11 +2,30 @@ from torch import nn
 import torch.nn.functional as F
 
 
-class BaseModel(nn.Module):
+class BaseClassifier(nn.Module):
     def __init__(self, input_dim, output_dim, n_hidden_layers, layer_size, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.input_layer = nn.Linear(input_dim, layer_size)
-        self.hidden_layers = [nn.Linear(layer_size, layer_size) for _ in range(n_hidden_layers - 1)]
+        self.hidden_layers = nn.ModuleList([nn.Linear(layer_size, layer_size) for _ in range(n_hidden_layers - 1)])
+        self.output_layer = nn.Linear(layer_size, output_dim)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.input_layer(x)
+        x = F.relu(x)
+        for layer in self.hidden_layers:
+            x = layer(x)
+            x = F.relu(x)
+        x = self.output_layer(x)
+        x = self.sigmoid(x)
+        return x
+
+
+class BaseRegressor(nn.Module):
+    def __init__(self, input_dim, output_dim, n_hidden_layers, layer_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.input_layer = nn.Linear(input_dim, layer_size)
+        self.hidden_layers = nn.ModuleList([nn.Linear(layer_size, layer_size) for _ in range(n_hidden_layers - 1)])
         self.output_layer = nn.Linear(layer_size, output_dim)
 
     def forward(self, x):
@@ -16,3 +35,4 @@ class BaseModel(nn.Module):
             x = layer(x)
             x = F.relu(x)
         x = self.output_layer(x)
+        return x
